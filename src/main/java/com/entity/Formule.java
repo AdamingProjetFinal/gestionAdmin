425f98c3b08e5f1ea6b6b55c628b5073cc0575ce
Flavien <flavien.gomila@gmail.com>
2020-11-19 10:29:41 +0100

    [Add] Entity Formule

---
+++ src/main/java/com/entity/Formule.java
@@ -0,0 +1,37 @@
+package com.entity;
+
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Formule {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "FORMULE_ID")
+	private Long id;
+	
+	@Column(name = "FORMULE_DATE")
+	private ZonedDateTime date;
+	
+	//Taux à définir
+	@Column(name = "FORMULE_TAXE")
+	private Double taxe;
+	
+	@Column(name = "FORMULE_SPEMED")
+	private Long idSpecialiteMedecin;
+	
+
+}
