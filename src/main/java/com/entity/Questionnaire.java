917efdf9ab6d80b75a134859e335175135171e35
Flavien <flavien.gomila@gmail.com>
2020-11-19 10:49:06 +0100

    [Add] Entity Questionnaire & Question

---
+++ src/main/java/com/entity/Questionnaire.java
@@ -0,0 +1,36 @@
+package com.entity;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Questionnaire {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "QUESTIONNAIRE_ID")
+	private Long id;
+	
+	@ManyToMany
+	@JoinTable(
+		name = "QUESTION_QUESTIONNAIRE",
+		joinColumns = @JoinColumn(name = "QUESTIONNAIRE_ID"),
+		inverseJoinColumns = @JoinColumn(name = "QUESTION_ID"))
+	private List<Question> questions;
+
+}
