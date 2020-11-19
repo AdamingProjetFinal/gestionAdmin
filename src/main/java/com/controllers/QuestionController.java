55d55b39aa563132a54f8c3f5b7f7dadbf7f1d9a
Flavien <flavien.gomila@gmail.com>
2020-11-19 16:11:20 +0100

    [Add] Controllers

---
+++ src/main/java/com/controllers/QuestionController.java
@@ -0,0 +1,10 @@
+package com.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/Question")
+@CrossOrigin(origins = "*")
+public class QuestionController {
+
+}
---
+++ src/main/java/com/controllers/ReponseQuestionnaireController.java
@@ -0,0 +1,11 @@
+package com.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/ReponseQuestionnaire")
+@CrossOrigin(origins = "*")
+
+public class ReponseQuestionnaireController {
+
+}
